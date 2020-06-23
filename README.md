a5e7a24b8ac914e6c2a64eb8a813a5290b56d345
Brenton Horne <brentonhorne77@gmail.com>
2020-06-24 05:28:22 +1000

    adding extra tests

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Integration.jl
-This package should be treated as a second-rate alternative to the excellent QuadGK package. QuadGK provides more accurate integration for many problems, and also provides an error estimate which functions in this package do not. 
+
+This package should be treated as a second-rate alternative to the excellent QuadGK package. QuadGK provides more accurate integration for many problems, and also provides an error estimate which functions in this package do not.
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,32 +2,23 @@ using Integration, SpecialFunctions, Test
 
 # Gaussian curve test
 N_gauss_test = 10000;
-function expnx2(x)
-    exp.(-x.^2)
-end
-
 @testset "Gaussian curve" begin
-    @test simpsons(expnx2, N_gauss_test, 0, 100) ≈ sqrt(pi)/2
-    @test trapezoidal(expnx2, N_gauss_test, 0, 100) ≈ sqrt(pi)/2
-    @test legendre_quadrature(expnx2, N_gauss_test, 0, 100) ≈ sqrt(pi)/2
-    @test abs(chebyshev_quadrature(expnx2, 1, N_gauss_test, 0, 100) - sqrt(pi)/2) < 1e-4
-    @test abs(chebyshev_quadrature(expnx2, 2, N_gauss_test, 0, 100) - sqrt(pi)/2) < 1e-4
+    @test simpsons(x -> exp.(-x.^2), N_gauss_test, 0, 100) ≈ sqrt(pi)/2
+    @test trapezoidal(x -> exp.(-x.^2), N_gauss_test, 0, 100) ≈ sqrt(pi)/2
+    @test legendre_quadrature(x -> exp.(-x.^2), N_gauss_test, 0, 100) ≈ sqrt(pi)/2
+    @test abs(chebyshev_quadrature(x -> exp.(-x.^2), 1, N_gauss_test, 0, 100) - sqrt(pi)/2) < 1e-4
+    @test abs(chebyshev_quadrature(x -> exp.(-x.^2), 2, N_gauss_test, 0, 100) - sqrt(pi)/2) < 1e-4
 end
 
 # Simple pendulum test
-simppen_analytical = ellipk(1/2)/sqrt(2.45); 
-function simppen(x)
-    (-19.6*sin.(x)).^(-0.5)
-end
-
 @testset "Simple pendulum" begin
     # Singularities exist at the endpoints so Simpson's & the trapezoidal rule cannot start and end at them exactly
-    @test abs(simpsons(simppen, 100000000, -pi+1e-8, -1e-8) - simppen_analytical) < 1e-4
-    @test abs(trapezoidal(simppen, 100000000, -pi+1e-8, -1e-8) - simppen_analytical) < 1e-4
-    @test abs(legendre_quadrature(simppen, 10000, -pi, 0) - simppen_analytical) < 1e-4
-    @test chebyshev_quadrature(simppen, 1, 100, -pi, 0) ≈ simppen_analytical
+    @test abs(simpsons(x -> (-19.6*sin.(x)).^(-0.5), 100000000, -pi+1e-8, -1e-8) - ellipk(1/2)/sqrt(2.45)) < 1e-4
+    @test abs(trapezoidal(x -> (-19.6*sin.(x)).^(-0.5), 100000000, -pi+1e-8, -1e-8) - ellipk(1/2)/sqrt(2.45)) < 1e-4
+    @test abs(legendre_quadrature(x -> (-19.6*sin.(x)).^(-0.5), 10000, -pi, 0) - ellipk(1/2)/sqrt(2.45)) < 1e-4
+    @test chebyshev_quadrature(x -> (-19.6*sin.(x)).^(-0.5), 1, 100, -pi, 0) ≈ ellipk(1/2)/sqrt(2.45)
     # Can't use ≈ for the 2nd Chebyshev quadrature as there's too much error in its estimate for that to work
-    @test abs(chebyshev_quadrature(simppen, 2, 10000, -pi, 0) - simppen_analytical) < 1e-3
+    @test abs(chebyshev_quadrature(x -> (-19.6*sin.(x)).^(-0.5), 2, 10000, -pi, 0) - ellipk(1/2)/sqrt(2.45)) < 1e-3
 end
 
 # cosine test
@@ -40,10 +31,34 @@ end
 end
 
 # logarithm test
-@testset "Logarithm test" begin
+@testset "Logarithm" begin
     @test abs(simpsons(x -> x.^(-1), 1000, 1, exp(1))-1) < 1e-10
     @test abs(trapezoidal(x -> x.^(-1), 100000, 1, exp(1))-1) < 1e-10
     @test abs(legendre_quadrature(x -> x.^(-1), 10000, 1, exp(1))-1) < 1e-10
     @test abs(chebyshev_quadrature(x -> x.^(-1), 1, 1000, 1, exp(1))-1) < 1e-5
     @test abs(chebyshev_quadrature(x -> x.^(-1), 2, 1000, 1, exp(1))-1) < 1e-5
+end
+
+# test an unusual trig integral
+@testset "cos_cot_integral" begin
+    @test simpsons(x -> (cos.(x).^2)/(1+cot.(x)), 10000, 0, pi/2) ≈ 0.25
+    @test trapezoidal(x -> (cos.(x).^2)/(1+cot.(x)), 10000, 0, pi/2) ≈ 0.25
+    # No Gaussian quadrature tests as they fail miserably and give results close to 0
+end
+
+@testset "log(x)/x" begin
+    @test simpsons(x -> log.(x).*x.^(-1), 10000, 1, exp(1)) ≈ 0.5
+    @test trapezoidal(x -> log.(x).*x.^(-1), 10000, 1, exp(1)) ≈ 0.5
+    @test legendre_quadrature(x -> log.(x).*x.^(-1), 10000, 1, exp(1)) ≈ 0.5
+    @test chebyshev_quadrature(x -> log.(x).*x.^(-1), 1, 10000, 1, exp(1)) ≈ 0.5
+    @test chebyshev_quadrature(x -> log.(x).*x.^(-1), 2, 10000, 1, exp(1)) ≈ 0.5
+end
+
+function test_7(x)
+    ((x.^3).+1.0)/((x.^4).*(x.+1.0).*((x.^2).+1.0))
+end
+sol_7 = log(sqrt(2)*exp(1)/(sqrt(exp(2)+1)))+1/2*(exp(-2)-1)+1/3*(1-exp(-3));
+@testset "(x^3+1)/((x^4)(x+1)(x^2+1))" begin
+    @test simpsons(test_7, 10000, 1, exp(1)) ≈ sol_7
+    @test trapezoidal(test_7, 10000, 1, exp(1)) ≈ sol_7
 end
