6529b4d1a08f552d76da7a5414b38506823a12b1
Brenton Horne <brentonhorne77@gmail.com>
2020-06-25 01:37:01 +1000

    Using FastGaussQuadrature to compute Legendre weights/zeros

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,12 @@
 using Integration, SpecialFunctions, Test
 
 # Gaussian curve test
-N_gauss_test = 10000;
 @testset "Gaussian curve" begin
-    @test simpsons(x -> exp.(-x.^2), N_gauss_test, 0, 100) ≈ sqrt(pi)/2
-    @test trapezoidal(x -> exp.(-x.^2), N_gauss_test, 0, 100) ≈ sqrt(pi)/2
-    @test legendre_quadrature(x -> exp.(-x.^2), N_gauss_test, 0, 100) ≈ sqrt(pi)/2
-    @test abs(chebyshev_quadrature(x -> exp.(-x.^2), 1, N_gauss_test, 0, 100) - sqrt(pi)/2) < 1e-4
-    @test abs(chebyshev_quadrature(x -> exp.(-x.^2), 2, N_gauss_test, 0, 100) - sqrt(pi)/2) < 1e-4
+    @test simpsons(x -> exp.(-x.^2), 10000, 0, 100) ≈ sqrt(pi)/2
+    @test trapezoidal(x -> exp.(-x.^2), 10000, 0, 100) ≈ sqrt(pi)/2
+    @test legendre_quadrature(x -> exp.(-x.^2), 10000, 0, 100) ≈ sqrt(pi)/2
+    @test chebyshev_quadrature(x -> exp.(-x.^2), 1, 100000, 0, 100) ≈ sqrt(pi)/2
+    @test chebyshev_quadrature(x -> exp.(-x.^2), 2, 100000, 0, 100) ≈ sqrt(pi)/2
 end
 
 # Simple pendulum test
@@ -31,7 +30,7 @@ end
 end
 
 # logarithm test
-@testset "Logarithm" begin
+@testset "1/x" begin
     @test abs(simpsons(x -> x.^(-1), 1000, 1, exp(1))-1) < 1e-10
     @test abs(trapezoidal(x -> x.^(-1), 100000, 1, exp(1))-1) < 1e-10
     @test abs(legendre_quadrature(x -> x.^(-1), 10000, 1, exp(1))-1) < 1e-10
@@ -40,6 +39,8 @@ end
 end
 
 function cos_cot_fn(x)
+    # ^(-1) is used instead of / as / produces a matrix instead of
+    # a vector
     (cos.(x).^2).*(cot.(x).+1.0).^(-1)
 end
 
@@ -66,10 +67,29 @@ function test_7(x)
 end
 sol_7 = log(sqrt(2)*exp(1)/(sqrt(exp(2)+1)))+1/2*(exp(-2)-1)+1/3*(1-exp(-3));
 
-@testset "(x^3+1)/((x^4)(x+1)(x^2+1))" begin
+@testset "test_7" begin
     @test simpsons(test_7, 10000, 1, exp(1)) ≈ sol_7
     @test trapezoidal(test_7, 100000, 1, exp(1)) ≈ sol_7
     @test legendre_quadrature(test_7, 10000, 1, exp(1)) ≈ sol_7
     @test chebyshev_quadrature(test_7, 1, 10000, 1, exp(1)) ≈ sol_7
     @test chebyshev_quadrature(test_7, 2, 100000, 1, exp(1)) ≈ sol_7
+end
+
+# Sinc function
+function sinxx(x)
+    if x != 0
+        return sin.(x).*x.^(-1)
+    else
+        return 1
+    end
+end
+
+sol_8 = 1.562225466889056293352345138804502677227824980541083456384;
+
+@testset "sinxx" begin
+    @test simpsons(sinxx, 100000, 0, 100) ≈ sol_8
+    @test trapezoidal(sinxx, 100000, 0, 100) ≈ sol_8
+    @test legendre_quadrature(sinxx, 10000, 0, 100) ≈ sol_8
+    @test chebyshev_quadrature(sinxx, 1, 100000, 0, 100) ≈ sol_8
+    @test chebyshev_quadrature(sinxx, 2, 100000, 0, 100) ≈ sol_8
 end
