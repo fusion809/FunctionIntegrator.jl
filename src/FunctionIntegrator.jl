750904ba7692272d6ab55eeb847bb81c94e759e8
Brenton Horne <brentonhorne77@gmail.com>
2020-07-22 20:37:43 +1000

    +new branch with Romberg's method; very inefficient, N>30 freezes

--- src/FunctionIntegrator.jl
+++ src/FunctionIntegrator.jl
@@ -2,7 +2,7 @@ module FunctionIntegrator
 
 using FastGaussQuadrature
 
-export chebyshev_quadrature, hermite_quadrature, jacobi_quadrature, laguerre_quadrature, legendre_quadrature, lobatto_quadrature, radau_quadrature, rectangle_rule_left, rectangle_rule_midpoint, rectangle_rule_right, simpsons_rule, simpsons38_rule, adaptive_simpsons_rule, trapezoidal_rule
+export chebyshev_quadrature, hermite_quadrature, jacobi_quadrature, laguerre_quadrature, legendre_quadrature, lobatto_quadrature, radau_quadrature, rectangle_rule_left, rectangle_rule_midpoint, rectangle_rule_right, rombergs_method, simpsons_rule, simpsons38_rule, adaptive_simpsons_rule, trapezoidal_rule
 
 include("Chebyshev.jl")
 include("Hermite.jl")
@@ -12,6 +12,7 @@ include("Legendre.jl")
 include("Lobatto.jl")
 include("Radau.jl")
 include("Rectangle.jl")
+include("Romberg.jl")
 include("Simpson.jl")
 include("Trapezoidal.jl")
 end # module
